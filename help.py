5eb3fe05faafaaa658072165f0e726ddb58d6037
lildrakk <juanillortiz11@gmail.com>
2026-04-28 15:55:06 +0200

    Update help.py

--- help.py
+++ help.py
@@ -12,14 +12,14 @@ class HelpSelect(discord.ui.Select):
     def __init__(self):
 
         options = [
-            discord.SelectOption(label="Moderación", emoji="🟥"),
-            discord.SelectOption(label="Seguridad", emoji="🛡️"),
-            discord.SelectOption(label="Información", emoji="ℹ️"),
-            discord.SelectOption(label="Utilidad", emoji="🛠️"),
-            discord.SelectOption(label="Verificación", emoji="🧩"),
-            discord.SelectOption(label="Blacklist", emoji="🚫"),
-            discord.SelectOption(label="Giveaways", emoji="🎉"),
-            discord.SelectOption(label="Backups", emoji="📦"),
+            discord.SelectOption(label="Moderación", emoji=discord.PartialEmoji(name="moderacion", id=1483506627649994812)),
+            discord.SelectOption(label="Seguridad", emoji=discord.PartialEmoji(name="candado", id=1491537429889552514)),
+            discord.SelectOption(label="Información", emoji=discord.PartialEmoji(name="iinfo", id=1491858536895090708)),
+            discord.SelectOption(label="Utilidad", emoji=discord.PartialEmoji(name="anuncio", id=1483506577024614660)),
+            discord.SelectOption(label="Verificación", emoji=discord.PartialEmoji(name="ao_Tick", id=1485072554879357089, animated=True)),
+            discord.SelectOption(label="Blacklist", emoji=discord.PartialEmoji(name="ban", id=1483506748944810217)),
+            discord.SelectOption(label="Giveaways", emoji=discord.PartialEmoji(name="giveaway", id=1494074344341639188)),
+            discord.SelectOption(label="Backups", emoji=discord.PartialEmoji(name="estrella", id=1494342514444996638)),
         ]
 
         super().__init__(
@@ -39,7 +39,7 @@ class HelpSelect(discord.ui.Select):
 
         if categoria == "Moderación":
             embed = discord.Embed(
-                title="🟥 Moderación",
+                title="<:moderacion:1483506627649994812> Moderación",
                 description=(
                     "**/ban** — Sistema de expulsión permanente.\n"
                     "**/unban** — Sistema para revertir expulsiones.\n"
@@ -59,19 +59,19 @@ class HelpSelect(discord.ui.Select):
 
         elif categoria == "Seguridad":
             embed = discord.Embed(
-                title="🛡️ Seguridad",
+                title="<:candado:1491537429889552514> Seguridad",
                 description=(
-                    "**/antibots** — Sistema de bloqueo de bots desconocidos.\n"
+                    "**/antibots** — Sistema de bloqueo de bots no verificados.\n"
                     "**/antibots_config** — Configuración del sistema antibots.\n"
                     "**/antiflood** — Sistema de control de spam masivo.\n"
                     "**/antilinks** — Sistema de bloqueo de enlaces no permitidos.\n"
-                    "**/antilinks_whitelist** — Gestión de enlaces permitidos.\n"
+                    "**/antilinks_whitelist** — Gestión de usuarios y roles sin  restricciones.\n"
                     "**/antiraid** — Sistema de protección contra raids.\n"
                     "**/antiraid_config** — Configuración del sistema antiraid.\n"
                     "**/antialts** — Sistema contra cuentas alternativas.\n"
-                    "**/antiping** — Sistema de control de menciones masivas.\n"
+                    "**/antiping** — Sistema de control de menciones indeseadas.\n"
                     "**/antiping_objetivo** — Configuración del objetivo protegido.\n"
-                    "**/antiping_whitelist** — Gestión de usuarios permitidos.\n"
+                    "**/antiping_whitelist** — Gestión de usuarios y roles permitidos.\n"
                     "**/securityscan** — Análisis de seguridad del servidor."
                 ),
                 color=COLOR
@@ -79,7 +79,7 @@ class HelpSelect(discord.ui.Select):
 
         elif categoria == "Información":
             embed = discord.Embed(
-                title="ℹ️ Información",
+                title="<:iinfo:1491858536895090708> Información",
                 description=(
                     "**/botinfo** — Información del bot.\n"
                     "**/serverinfo** — Información del servidor.\n"
@@ -90,7 +90,7 @@ class HelpSelect(discord.ui.Select):
 
         elif categoria == "Utilidad":
             embed = discord.Embed(
-                title="🛠️ Utilidad",
+                title="<:anuncio:1483506577024614660> Utilidad",
                 description=(
                     "**/say** — Enviar un mensaje mediante el bot.\n"
                     "**/spoiler** — Enviar un spoiler formateado.\n"
@@ -101,7 +101,7 @@ class HelpSelect(discord.ui.Select):
 
         elif categoria == "Verificación":
             embed = discord.Embed(
-                title="🧩 Verificación",
+                title="<a:ao_Tick:1485072554879357089> Verificación",
                 description=(
                     "**/verificacion** — Sistema de verificación del servidor.\n"
                     "**/verificacion_enviar** — Enviar el panel de verificación."
@@ -111,7 +111,7 @@ class HelpSelect(discord.ui.Select):
 
         elif categoria == "Blacklist":
             embed = discord.Embed(
-                title="🚫 Blacklist",
+                title="<:ban:1483506748944810217> Blacklist",
                 description=(
                     "**/global_blacklist** — Gestión de la blacklist global.\n"
                     "**/blacklist** — Añadir usuarios a la blacklist.\n"
@@ -123,7 +123,7 @@ class HelpSelect(discord.ui.Select):
 
         elif categoria == "Giveaways":
             embed = discord.Embed(
-                title="🎉 Giveaways",
+                title="<:giveaway:1494074344341639188> Giveaways",
                 description=(
                     "**/giveaway** — Crear un sorteo.\n"
                     "**/reroll** — Repetir ganador."
@@ -133,7 +133,7 @@ class HelpSelect(discord.ui.Select):
 
         elif categoria == "Backups":
             embed = discord.Embed(
-                title="📦 Backups",
+                title="<:estrella:1494342514444996638> Backups",
                 description=(
                     "**/backup_crear** — Crear una copia del servidor.\n"
                     "**/backup_restaurar** — Restaurar un backup.\n"
@@ -169,7 +169,7 @@ class Help(commands.Cog):
     async def help(self, interaction: discord.Interaction):
 
         embed = discord.Embed(
-            title="📘 Panel de Ayuda — ModdyBot",
+            title="<:reglas:1494341139107418253> Panel de Ayuda — ModdyBot",
             description=(
                 "Selecciona una categoría en el menú inferior para ver los comandos disponibles.\n\n"
                 "ModdyBot ofrece sistemas avanzados de moderación, seguridad, verificación, backups y más."
@@ -177,7 +177,7 @@ class Help(commands.Cog):
             color=COLOR
         )
 
-        await interaction.response.send_message(embed=embed, view=HelpView(), ephemeral=True)
+        await interaction.response.send_message(embed=embed, view=HelpView(), ephemeral=False)
 
 
 async def setup(bot):
